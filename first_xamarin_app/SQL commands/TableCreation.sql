7bd05029d05af0ca3436dc3e0af4c64a9ed2619b
Troemer16 <troemer@mail.greenriver.edu>
2019-10-18 05:54:13 -0700

    Added SQLite dependency and created sql commands for practice data

---
+++ first_xamarin_app/SQL commands/TableCreation.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Employee (
+    EmployeeID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name VARCHAR(50),
+    Age INT(3),
+    Position VARCHAR(100)
+);
---
+++ first_xamarin_app/SQL commands/TablePopulation.sql
@@ -0,0 +1,4 @@
+INSERT INTO Employee(Name, Age, Position) VALUES
+     ('John Doe', 34, 'Manager'),
+     ('Ron Burgundy', 23, 'Software Developer'),
+     ('John Appleseed', 24, 'Tester');
